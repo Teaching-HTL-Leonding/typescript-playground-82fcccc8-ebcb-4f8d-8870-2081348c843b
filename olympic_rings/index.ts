92c34499237d3210bff7846a3e84ea4627896485
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 08:26:40 +0200

    Saved in TypeScript Playground at 2024-09-19T06:26:39.776Z

---
+++ olympic_rings/index.ts
@@ -0,0 +1,10 @@
+function setup() {
+createCanvas(600, 800);
+background("white");
+noFill();
+circle(300, 200, 250);
+circle(200, 200, 250);
+circle(200, 200, 250);
+circle(200, 200, 250);
+circle(200, 200, 250);
+}
