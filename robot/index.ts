74fd674207ad76e5dcbfcb3d77e4cd41010f7754
Rainer Stropek <rainer@timecockpit.com>
2024-09-25 22:09:07 +0200

    Saved in TypeScript Playground at 2024-09-25T20:09:06.838Z

--- robot/index.ts
+++ robot/index.ts
@@ -1,20 +1,32 @@
 function setup() {
-
     createCanvas(3000, 3000);
+    background("rgb(0, 255, 255)");
 
+    fill("lightgrey");
+    circle(400, 400, 300);
 
-    background("lightgrey");
-
-fill("grey");
-    rect(100, 500, 400);
 
     fill("black");
-    circle(150, 600, 70);
-   circle(450, 600, 70);
+    circle(345, 390, 30);
+    circle(455, 390, 30);
+      fill("HotPink");
+    triangle(385, 430, 425, 430, 405, 445);
+       strokeWeight(2);
+    stroke("black");
+    line(320, 470, 390, 470);
+    line(420, 470, 490, 470);
+    line(405, 445, 405, 485);
+    line(320, 440, 390, 460);
+    line(420, 460, 490, 440);
+    line(405,485, 435, 500);
+    line(405, 485, 375, 500);
+    line(435, 500, 490, 480 );
+    line(375, 500, 320, 480 );
 
-   fill("white");
-   rect(200, 750, 200, 40);
-   line(300, 500, 300, 400);
-   circle(300, 355, 90);
+        fill("lightgray");
+    triangle(250, 320, 300, 275, 260, 180);
+    triangle(550, 320, 500, 275, 560, 180);
+
+  
 
 }
