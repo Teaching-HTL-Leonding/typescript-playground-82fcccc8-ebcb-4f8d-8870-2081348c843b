7ad0f9a1d99e31d4d1a8982e9254ca594efd2208
Rainer Stropek <rainer@timecockpit.com>
2024-10-04 12:39:52 +0200

    Saved in TypeScript Playground at 2024-10-04T10:39:51.658Z

---
+++ color_areas/index.ts
@@ -0,0 +1,30 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    noStroke();
+
+
+}
+
+// <<< Add the function `moveClicked` with the required code here
+{
+
+
+    let ajdin: string;
+
+    if (mouseX < width / 3) {
+        ajdin = "Yellow";
+    } else if (mouseX < width / 3 * 2) {
+        
+        ajdin = "Green";
+    } else {
+        
+        ajdin ="Red";
+    }
+
+
+}
+
+
+
+
