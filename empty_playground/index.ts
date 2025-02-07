dddcc5046b087469863a5a65e97f2cfd6d8f8740
Rainer Stropek <rainer@timecockpit.com>
2025-02-07 12:24:46 +0100

    Saved in TypeScript Playground at 2025-02-07T11:24:45.904Z

---
+++ empty_playground/index.ts
@@ -0,0 +1,15 @@
+let message = "lets get started";
+
+function sayHello() {
+    let message = "Hello";
+     console.log(message);
+}
+
+function sayHi() {
+    let message = "Hi";
+    console.log(message);
+}
+
+sayHello();
+sayHi();
+console.log(message);
